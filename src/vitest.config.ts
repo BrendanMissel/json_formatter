8cfe235a3cf01ed7a676e57ffe0ef8a3cbc1ff49
Brendan Missel <brendan@missel.dev>
2026-03-03 21:35:22 -0600

    Cleanup src (#18)
    * nest under app to clean up root directory
    
    * Update GitHub Actions workflows to set working directory for npm commands
    
    * all node commands apparantly, and remove comments
    
    * order maybe? idk yaml
    
    * try again
    
    * update linter ref

--- src/vitest.config.ts
+++ src/vitest.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'happy-dom',
-    setupFiles: ['./src/test/setup.ts'],
+    setupFiles: ['./app/test/setup.ts'],
     pool: 'threads',
     globals: true,
   },
-})
+});
